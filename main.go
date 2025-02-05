fc9c2eed6f8587a7408a6f4192834edbe0116dcf
Arailym Mukazhan <153664222+araichok@users.noreply.github.com>
2025-02-05 21:49:16 +0500

    Update main.go

--- main.go
+++ main.go
@@ -1,24 +1,36 @@
 package main
 
 import (
-	"awesomeProject17/handlers"
 	"log"
 	"net/http"
 
+	"YurtMart/database"
+	"YurtMart/routes"
+	"YurtMart/web"
 	"github.com/gorilla/mux"
-
-	"awesomeProject17/config"
 )
 
 func main() {
-	config.ConnectDB()
+
+	if err := database.ConnectDB(); err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+	defer func() {
+		if err := database.DisconnectDB(); err != nil {
+			log.Printf("Error disconnecting from database: %v", err)
+		}
+	}()
+
+	database.Connect_DB()
 
 	router := mux.NewRouter()
-	router.HandleFunc("/items", handlers.GetItems).Methods("GET")
-	router.HandleFunc("/items", handlers.CreateItem).Methods("POST")
-	router.HandleFunc("/items", handlers.UpdateItem).Methods("PUT")
-	router.HandleFunc("/items", handlers.DeleteItem).Methods("DELETE")
 
-	log.Println("Server started on :8081")
-	log.Fatal(http.ListenAndServe(":8081", router))
+	routes.RegisterRoutes()
+	routes.RegisterItemRoutes(router)
+	routes.RegisterOrderRoutes(router)
+
+	web.SetupTemplates()
+
+	log.Println("Server started on :8087")
+	log.Fatal(http.ListenAndServe(":8087", router))
 }
